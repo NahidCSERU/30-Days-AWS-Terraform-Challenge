76764cb410dc134236ab58df678b044aafee52fc
NahidCSERU <nahid.hasancseru@gmail.com>
2025-08-18 22:52:54 +0600

    feat(vpc): fetch default VPC and subnet IDs

--- day02-ec2-basic/main.tf
+++ day02-ec2-basic/main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = var.aws_region
 }
+# Get default VPC (most beginners use this)
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get default subnet
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
