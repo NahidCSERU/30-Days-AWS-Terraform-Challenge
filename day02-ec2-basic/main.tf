bb348f61f9b5e256179abf36c825756605fd7b50
NahidCSERU <nahid.hasancseru@gmail.com>
2025-08-18 23:03:32 +0600

    feat(ec2): launch t2.micro EC2 instance with public IP

--- day02-ec2-basic/main.tf
+++ day02-ec2-basic/main.tf
@@ -31,3 +31,15 @@ data "aws_vpc" "default" {
 data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
+# EC2 instance
+resource "aws_instance" "ec2_example" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = element(data.aws_subnet_ids.default.ids, 0)
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "day02-ec2-instance"
+  }
+}
