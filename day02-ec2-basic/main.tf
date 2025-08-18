b1f820c4c831e8a978ec286ea2cffbb1b6d04c45
NahidCSERU <nahid.hasancseru@gmail.com>
2025-08-18 23:00:00 +0600

    feat(security): create security group with SSH (22) access

--- day02-ec2-basic/main.tf
+++ day02-ec2-basic/main.tf
@@ -1,6 +1,27 @@
 provider "aws" {
   region = var.aws_region
 }
+# Create a security group
+resource "aws_security_group" "ec2_sg" {
+  name        = "day02-ec2-sg"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # Get default VPC (most beginners use this)
 data "aws_vpc" "default" {
   default = true
