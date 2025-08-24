9de4f737ca3edb66decbd0ef00162108ab871d4b
NahidCSERU <nahid.hasancseru@gmail.com>
2025-08-24 19:59:12 +0600

    feat(terraform): day 08 create vpc with cidr block

---
+++ day08-vpc/variables.tf
@@ -0,0 +1,19 @@
+# Input variables for VPC project
+
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "day08-demo-vpc"
+}
