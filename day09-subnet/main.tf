61a560afc7539dfd3ec9ae2c90619006ed275231
NahidCSERU <nahid.hasancseru@gmail.com>
2025-08-25 20:24:15 +0600

    feat(main.tf): day 09 create vpc and set provider

---
+++ day09-subnet/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.aws_region
+}
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "day09-main-vpc"
+  }
+}
