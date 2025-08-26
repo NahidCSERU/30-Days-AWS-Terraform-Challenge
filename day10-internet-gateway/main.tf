927f14b48e158fab694670fd0268ad00c91c1636
NahidCSERU <nahid.hasancseru@gmail.com>
2025-08-26 20:01:35 +0600

    feat(vpc): day 10 Create Internet Gateway and attached vpc

--- day10-internet-gateway/main.tf
+++ day10-internet-gateway/main.tf
@@ -10,3 +10,14 @@ resource "aws_vpc" "main" {
     Name = "${var.project_name}-vpc"
   }
 }
+
+# -------------------
+# Create Internet Gateway
+# -------------------
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
