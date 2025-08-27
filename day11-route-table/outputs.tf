34f08ba19ac3f805b22b78f045443963ff64a895
NahidCSERU <nahid.hasancseru@gmail.com>
2025-08-27 18:38:26 +0600

    feat(output.tf): day 11 outputs file updated

---
+++ day11-route-table/outputs.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "route_table_id" {
+  value = aws_route_table.public_rt.id
+}
