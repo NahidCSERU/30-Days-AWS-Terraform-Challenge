87c076375781cb45bf63b0e8ee78de2995946dc0
NahidCSERU <nahid.hasancseru@gmail.com>
2025-08-27 18:36:18 +0600

    feat(main.tf): day 11 Internet Gateway and public subnet create

--- day11-route-table/main.tf
+++ day11-route-table/main.tf
@@ -7,3 +7,16 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = { Name = "day11-vpc" }
 }
+# 2. Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = { Name = "day11-igw" }
+}
+# 3. Public Subnet
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+  tags = { Name = "public-subnet" }
+}
