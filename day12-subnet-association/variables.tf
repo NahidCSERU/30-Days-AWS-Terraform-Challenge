e7dc7d1fd77daa01870fe86c2c7faacea29602d1
NahidCSERU <nahid.hasancseru@gmail.com>
2025-08-28 19:26:33 +0600

    feat(provider.tf): day 12 provider and varibles set

---
+++ day12-subnet-association/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for Subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "AZ for subnet"
+  type        = string
+  default     = "us-east-1a"
+}
