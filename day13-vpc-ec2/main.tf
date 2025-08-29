a0764aefa7c22a6507e58aa9c965678b49f26459
NahidCSERU <nahid.hasancseru@gmail.com>
2025-08-29 21:41:22 +0600

    feat(main.tf): route table+ SG added

--- day13-vpc-ec2/main.tf
+++ day13-vpc-ec2/main.tf
@@ -34,3 +34,61 @@ resource "aws_internet_gateway" "igw" {
     Name = "day13-igw"
   }
 }
+
+# ------------------------
+# Route Table (Public)
+# ------------------------
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "day13-public-rt"
+  }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# ------------------------
+# Security Group
+# ------------------------
+resource "aws_security_group" "public_sg" {
+  name        = "day13-public-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "day13-public-sg"
+  }
+}
