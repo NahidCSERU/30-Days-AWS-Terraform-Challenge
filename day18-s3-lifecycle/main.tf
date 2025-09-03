b564bf3e8d22cde9c8fa808b8e6314bdc740ecea
NahidCSERU <nahid.hasancseru@gmail.com>
2025-09-03 19:29:16 +0600

    feat(main.tf): attached policy

--- day18-s3-lifecycle/main.tf
+++ day18-s3-lifecycle/main.tf
@@ -34,3 +34,20 @@ resource "aws_s3_bucket" "my_bucket" {
     }
   }
 }
+
+# Optional: S3 bucket policy to allow public read (example)
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = ["s3:GetObject"]
+        Resource  = "${aws_s3_bucket.my_bucket.arn}/*"
+      }
+    ]
+  })
+}
---
+++ day18-s3-lifecycle/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS Region"
+  default     = "ap-southeast-1"
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name"
+  default     = "day18-s3-lifecycle-demo-12345"
+}
