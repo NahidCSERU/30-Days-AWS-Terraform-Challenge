30d9d581e63e4d2bb4f93c6c21dcdae29788c441
NahidCSERU <nahid.hasancseru@gmail.com>
2025-09-03 19:34:18 +0600

    feat(main.tf): update main file

--- day18-s3-lifecycle/main.tf
+++ day18-s3-lifecycle/main.tf
@@ -5,34 +5,6 @@ provider "aws" {
 # Create S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
-    id      = "archive-old-objects"
-    enabled = true
-
-    prefix = ""   # Apply to all objects
-    tags = {
-      "auto-archive" = "true"
-    }
-
-    transition {
-      days          = 30
-      storage_class = "STANDARD_IA" # Infrequent Access
-    }
-
-    transition {
-      days          = 60
-      storage_class = "GLACIER"    # Glacier archive
-    }
-
-    expiration {
-      days = 365   # Delete after 1 year
-    }
-  }
 }
 
 # Optional: S3 bucket policy to allow public read (example)
