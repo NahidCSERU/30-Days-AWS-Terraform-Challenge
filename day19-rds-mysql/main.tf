e8f607997e7c2be50d0f91456a56a6efc7d7c326
NahidCSERU <nahid.hasancseru@gmail.com>
2025-09-04 20:00:12 +0600

    feat(main): main script for rds

---
+++ day19-rds-mysql/main.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Security group for RDS
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-sg"
+  description = "Allow MySQL access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_cidrs
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-sg"
+  }
+}
+
+# Subnet group
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group"
+  subnet_ids = var.subnet_ids
+  tags = {
+    Name = "rds-subnet-group"
+  }
+}
+
+# RDS MySQL instance
+resource "aws_db_instance" "mydb" {
+  identifier              = var.db_name
+  allocated_storage       = var.db_allocated_storage
+  engine                  = "mysql"
+  engine_version          = "8.0"
+  instance_class          = var.db_instance_class
+  name                    = var.db_name
+  username                = var.db_username
+  password                = var.db_password
+  db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+  skip_final_snapshot     = true
+  publicly_accessible     = var.publicly_accessible
+  deletion_protection     = false
+  multi_az                = false
+  auto_minor_version_upgrade = true
+
+  tags = {
+    Name = "MySQL-RDS"
+  }
+}
